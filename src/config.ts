391d7d0c2b10b1d81b271be39a4fbfd966e7d4fb
nigmaz <trantronghuy0706@gmail.com>
2025-08-07 14:31:01 +0700

    Automated commit: Thu 08/07/2025 14:29:42.21

--- src/config.ts
+++ src/config.ts
@@ -57,7 +57,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "nigmaz",
-	bio: "Exploit Development | Reverse Engineering | Malware Analysis | ZeroDay Research",
+	bio: "Pwnable Exploit | Reverse Engineering | Malware Analysis | ZeroDay Research and Development",
 	links: [
 		{
 			name: "Twitter",
